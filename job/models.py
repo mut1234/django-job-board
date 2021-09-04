f41ba140da27def6ace0f26a58655981919bbed3
Mut1234 <d50dv4@gmail.com>
2021-09-04 16:09:21 +0300

    add job views& url

--- job/models.py
+++ job/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+'''
+django model filed:
+-html widget
+-validation
+-db size
+'''
+
 # Create your models here.
+
+JOB_TYPE=(
+    ('Full Iime','Full Iime'),
+    ('Part Iime','Part Iime'),
+
+)
+class Job(models.Model): #table
+    title = models.CharField(max_length=100) #column
+    job_type = models.CharField(max_length=15 ,choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True) #auto_now time of add \ auto_now_add time of last edit
+    Vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    experience = models.IntegerField(default=1)
+    category = models.ForeignKey('Category',on_delete=models.CASCADE) # 1 to many
+    def __str__(self): 
+        return self.title # to return title instead than object
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+    def __str__(self):
+        return self.name
--- job/views.py
+++ job/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def job_list(request):
+    pass
+
+
+def job_detiles(request,id):
+    pass
+
+
