1c808dbf4b869cd2b643a5132e3a5e0cc1e641b8
Mut1234 <d50dv4@gmail.com>
2021-09-12 17:58:55 +0300

    fix pagination

--- job/views.py
+++ job/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator
 # Create your views here.
 def job_list(request):
     job_list=Job.objects.all()
-    print(job_list)
-    context={'jobs' :job_list}
+    paginator = Paginator(job_list, 1) # Show 25 
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    context={'jobs' :page_obj}
     return render(request,'job/job_list.html',context)
 
 
