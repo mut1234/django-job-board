f13ff346ea1a0ed81dbaa2b34cb35b679d16aec6
Mut1234 <d50dv4@gmail.com>
2021-09-14 13:57:39 +0300

    Add login , logout , password change

--- project/settings.py
+++ project/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 
 
 INSTALLED_APPS = [
+    'accounts',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 
     # my apps
     'job',
+  
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -18,6 +18,7 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
     path('jobs/', include('job.urls',namespace='jobs')),
     
