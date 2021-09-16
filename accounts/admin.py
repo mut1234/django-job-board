1744e2dc4911f41a593479beaa087d11a1bec440
Mut1234 <d50dv4@gmail.com>
2021-09-16 17:32:05 +0300

    continue in login & rest password and confirm & add signup with signals to profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Profile,City
 # Register your models here.
+
+
+admin.site.register(City)
+admin.site.register(Profile)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,28 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
+
+
+class Profile(models.Model):
+    user=models.OneToOneField(User,  on_delete=models.CASCADE)
+    city= models.ForeignKey('City',related_name='user_city' ,on_delete=models.CASCADE,blank=True,null=True)
+    phone_number=models.CharField(max_length=15)
+    image = models.ImageField(upload_to='profile/')
+
+    def __str__(self): 
+        return str(self.user)
+
+
+
+@receiver(post_save, sender=User) ##post after signup its driect create profile
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+## create new user ---> create new empty profile
+class City(models.Model):
+    name=models.CharField(max_length=30)
+
